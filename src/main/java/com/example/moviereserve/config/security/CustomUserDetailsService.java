6a73c302af3f92cb9e0471fb2b51b7b32055a353
CordHouse <qkqktl5310@gmail.com>
2023-06-14 02:26:37 +0900

    feat : UserDetailsService 구현

--- src/main/java/com/example/moviereserve/config/security/CustomUserDetailsService.java
+++ src/main/java/com/example/moviereserve/config/security/CustomUserDetailsService.java
@@ -1,10 +1,12 @@
 package com.example.moviereserve.config.security;
 
 import com.example.moviereserve.entity.User;
+import com.example.moviereserve.exception.NotFoundUserException;
 import com.example.moviereserve.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,7 @@ import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
