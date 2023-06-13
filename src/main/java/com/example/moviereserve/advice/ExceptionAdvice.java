498070c8fbea7bbaa97255d6c63fb46d658a9d77
CordHouse <qkqktl5310@gmail.com>
2023-06-14 02:18:44 +0900

    feat : 에러 처리 init

---
+++ src/main/java/com/example/moviereserve/advice/ExceptionAdvice.java
@@ -0,0 +1,16 @@
+package com.example.moviereserve.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response methodArgumentNotValidException() {
+        return Response.failure(400, "유저를 찾을 수 없습니다.");
+    }
+}
