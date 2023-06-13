5616485918428198f90c92f85f70887890241e8e
CordHouse <qkqktl5310@gmail.com>
2023-06-14 04:28:03 +0900

    feat : 사용자 회원가입 요청

---
+++ src/main/java/com/example/moviereserve/dto/UserSignUpRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.moviereserve.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSignUpRequestDto {
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
