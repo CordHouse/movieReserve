229545a4aa72e202059cafdda967be7b47b6c17a
CordHouse <qkqktl5310@gmail.com>
2023-06-14 01:50:13 +0900

    Initial commit

---
+++ src/main/java/com/example/moviereserve/MovieReserveApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviereserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReserveApplication.class, args);
+	}
+
+}
