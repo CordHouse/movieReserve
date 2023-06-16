c8e1eac69f484ad8ac0c11598adef93c3a96f7b1
CordHouse <qkqktl5310@gmail.com>
2023-06-16 15:19:01 +0900

    feat : 공연장 서비스 등록

---
+++ src/main/java/com/example/moviereserve/service/VenuesService.java
@@ -0,0 +1,29 @@
+package com.example.moviereserve.service;
+
+import com.example.moviereserve.dto.VenuesResponseDto;
+import com.example.moviereserve.dto.VenuesSetupRequestDto;
+import com.example.moviereserve.entity.User;
+import com.example.moviereserve.entity.Venues;
+import com.example.moviereserve.entity.VenuesType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class VenuesService {
+    private final VenuesRepository venuesRepository;
+
+    @Transactional
+    public VenuesResponseDto venuesSetup(VenuesSetupRequestDto venuesSetupRequestDto, User user) {
+        Venues venues = new Venues(
+                venuesSetupRequestDto.getName(),
+                venuesSetupRequestDto.getCapacity(),
+                VenuesType.findVenuesType(venuesSetupRequestDto.getVenuesType()),
+                venuesSetupRequestDto.getStartTime(),
+                venuesSetupRequestDto.getEndTime());
+        venuesRepository.save(venues);
+
+        return new VenuesResponseDto().toDo(venuesSetupRequestDto);
+    }
+}
