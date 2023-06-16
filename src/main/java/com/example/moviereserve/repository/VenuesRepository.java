2fe9a2ce6920902a54d59d670bc650b612560f16
CordHouse <qkqktl5310@gmail.com>
2023-06-16 14:38:29 +0900

    feat : 공연장 레포 초기화

---
+++ src/main/java/com/example/moviereserve/repository/VenuesRepository.java
@@ -0,0 +1,9 @@
+package com.example.moviereserve.repository;
+
+import com.example.moviereserve.entity.Venues;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VenuesRepository extends JpaRepository<Venues, Long> {
+}
