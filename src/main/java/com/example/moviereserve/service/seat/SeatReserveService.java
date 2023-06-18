eb00fee063ed36be7065346124d58071c99cf102
CordHouse <qkqktl5310@gmail.com>
2023-06-18 20:34:55 +0900

    refactor : 오타 수정, SeatInfoResponseDto 추가

--- src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
+++ src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
@@ -1,6 +1,7 @@
 package com.example.moviereserve.service.seat;
 
 import com.example.moviereserve.dto.seat.SeatInfo;
+import com.example.moviereserve.dto.seat.SeatInfoResponseDto;
 import com.example.moviereserve.dto.seat.SeatReserveRequestDto;
 import com.example.moviereserve.dto.seat.SeatReserveResponseDto;
 import com.example.moviereserve.entity.seat.Seat;
@@ -12,10 +13,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SeatReserveService {
-    private static final String RESERVED = "occupied"
+    private static final String RESERVED = "occupied";
     private final SeatRepository seatRepository;
     private final VenuesRepository venuesRepository;
 
@@ -23,12 +27,15 @@ public class SeatReserveService {
     @Transactional
     public SeatReserveResponseDto reserve(SeatReserveRequestDto seatReserveRequestDto, User user) {
         Venues venues = venuesRepository.findById(seatReserveRequestDto.getPerformanceId()).orElseThrow();
+        List<SeatInfoResponseDto> seatInfoResponseDtoList = new ArrayList<>();
         for(SeatInfo reserve : seatReserveRequestDto.getSeats()) {
             Seat seat = seatRepository.findBySeatNumber(reserve.getSeatNumber()).orElseThrow();
             seat.setVenues(venues);
             seat.setUser(user);
             seat.setStatus(RESERVED);
+
+            seatInfoResponseDtoList.add(new SeatInfoResponseDto().toDo(seat));
         }
-        return new SeatReserveResponseDto().toDo(venues, seatReserveRequestDto.getSeats());
+        return new SeatReserveResponseDto().toDo(venues, seatInfoResponseDtoList);
     }
 }
