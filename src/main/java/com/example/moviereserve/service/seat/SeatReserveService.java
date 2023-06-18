0901736ac05d8dc517273b373cd766f2c9097163
CordHouse <qkqktl5310@gmail.com>
2023-06-18 20:21:55 +0900

    refactor : 좌석 예매 수정

--- src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
+++ src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
@@ -1,7 +1,13 @@
 package com.example.moviereserve.service.seat;
 
+import com.example.moviereserve.dto.seat.SeatInfo;
+import com.example.moviereserve.dto.seat.SeatReserveRequestDto;
+import com.example.moviereserve.dto.seat.SeatReserveResponseDto;
+import com.example.moviereserve.entity.seat.Seat;
 import com.example.moviereserve.entity.user.User;
+import com.example.moviereserve.entity.venues.Venues;
 import com.example.moviereserve.repository.SeatRepository;
+import com.example.moviereserve.repository.VenuesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,11 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class SeatReserveService {
+    private static final String RESERVED = "occupied"
     private final SeatRepository seatRepository;
+    private final VenuesRepository venuesRepository;
 
+    // 좌석 예매
     @Transactional
     public SeatReserveResponseDto reserve(SeatReserveRequestDto seatReserveRequestDto, User user) {
-
-        return new SeatReserveResponseDto.toDo();
+        Venues venues = venuesRepository.findById(seatReserveRequestDto.getPerformanceId()).orElseThrow();
+        for(SeatInfo reserve : seatReserveRequestDto.getSeats()) {
+            Seat seat = seatRepository.findBySeatNumber(reserve.getSeatNumber()).orElseThrow();
+            seat.setVenues(venues);
+            seat.setUser(user);
+            seat.setStatus(RESERVED);
+        }
+        return new SeatReserveResponseDto().toDo(venues, seatReserveRequestDto.getSeats());
     }
 }
