a33d8fa6b5b36c6b0000c3bd5bc0e6721e8d1921
CordHouse <qkqktl5310@gmail.com>
2023-06-18 20:31:51 +0900

    refactor : 타입 변경

--- src/main/java/com/example/moviereserve/dto/seat/SeatReserveResponseDto.java
+++ src/main/java/com/example/moviereserve/dto/seat/SeatReserveResponseDto.java
@@ -1,19 +1,21 @@
 package com.example.moviereserve.dto.seat;
 
 import com.example.moviereserve.entity.seat.Seat;
+import com.example.moviereserve.entity.venues.Venues;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SeatReserveResponseDto {
     private long performanceId;
-    private long seatId;
-    private String seatNumber;
+    private List<SeatInfoResponseDto> seats;
 
-    public SeatReserveResponseDto toDo(Seat seat) {
-        return new SeatReserveResponseDto(seat.getVenues().getId(), seat.getId(), seat.getSeatNumber());
+    public SeatReserveResponseDto toDo(Venues venues, List<SeatInfoResponseDto> seat) {
+        return new SeatReserveResponseDto(venues.getId(), seat);
     }
 }
