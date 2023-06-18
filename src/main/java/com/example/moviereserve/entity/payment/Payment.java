257a827f05d5e36d6dd04d400f0e8e9e10d7eacf
CordHouse <qkqktl5310@gmail.com>
2023-06-19 06:36:17 +0900

    refactor : 결제 도메인 수정 -> 카드정보 삽입

--- src/main/java/com/example/moviereserve/entity/payment/Payment.java
+++ src/main/java/com/example/moviereserve/entity/payment/Payment.java
@@ -1,14 +1,15 @@
 package com.example.moviereserve.entity.payment;
 
-import com.example.moviereserve.entity.payment.card.CardRegister;
 import com.example.moviereserve.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
@@ -20,14 +21,39 @@ public class Payment {
     private long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false, name = "card_id")
+    @JoinColumn(nullable = false, name = "user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private CardRegister card;
+    private User user;
 
     @Column(nullable = false)
-    private String price;
+    private String paymentMethod;
 
-    public Payment(String price) {
-        this.price = price;
+    @Column(nullable = false)
+    private String cardNumber;
+
+    @Column(nullable = false)
+    private String cardExpiration;
+
+    @Column(nullable = false)
+    private String cardCVV;
+
+    @Column(nullable = false)
+    private int totalPrice;
+
+    @DateTimeFormat(pattern = "YYYY-mm-dd HH:mm:ss")
+    private LocalDateTime localDateTime;
+
+    @PrePersist
+    public void createTime(){
+        this.localDateTime = LocalDateTime.now();
+    }
+
+    public Payment(User user, String paymentMethod, String cardNumber, String cardExpiration, String cardCVV, int totalPrice){
+        this.user = user;
+        this.paymentMethod = paymentMethod;
+        this.cardNumber = cardNumber;
+        this.cardExpiration = cardExpiration;
+        this.cardCVV = cardCVV;
+        this.totalPrice = totalPrice;
     }
 }
