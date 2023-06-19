2512d6ee99cc0856c581c6dcfe99bc807424d16c
CordHouse <qkqktl5310@gmail.com>
2023-06-20 04:34:00 +0900

    refactor : 어노테이션 변경 Blank->Empty

--- src/main/java/com/example/moviereserve/dto/seat/SeatReserveRequestDto.java
+++ src/main/java/com/example/moviereserve/dto/seat/SeatReserveRequestDto.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -15,7 +15,7 @@ import java.util.List;
 public class SeatReserveRequestDto {
     @NotNull(message = "공연 정보가 누락 되었습니다.")
     private long performanceId;
-    @NotBlank(message = "예매할 좌석이 누락 되었습니다.")
+    @NotEmpty(message = "예매할 좌석이 누락 되었습니다.")
     private List<SeatInfo> seats;
     @NotNull(message = "총 가격이 누락 되었습니다.")
     private int totalPrice;
