d72445a4029c115a8e51b636057f2c70cf51961c
CordHouse <qkqktl5310@gmail.com>
2023-06-20 04:34:27 +0900

    refactor : seatsInfo 삭제

--- src/main/java/com/example/moviereserve/dto/venues/VenuesResponseDto.java
+++ src/main/java/com/example/moviereserve/dto/venues/VenuesResponseDto.java
@@ -1,10 +1,10 @@
 package com.example.moviereserve.dto.venues;
 
+import com.example.moviereserve.dto.seat.SeatRegisterDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,28 +15,14 @@ public class VenuesResponseDto {
     private int capacity;
     private String venuesType;
     private String possibleTimes;
-    private List<SeatsResponseDto> seats;
+    private List<SeatRegisterDto> seats;
 
     public VenuesResponseDto toDo(VenuesSetupRequestDto venues) {
-        StringBuilder time = new StringBuilder();
-        time.append(venues.getStartTime()).append("-").append(venues.getEndTime());
         return new VenuesResponseDto(
                 venues.getName(),
                 venues.getCapacity(),
                 venues.getVenuesType(),
-                time.toString(),
-                seatsInfo(venues.getVipSeatMaxNumber(), venues.getCapacity()));
-    }
-
-    public List<SeatsResponseDto> seatsInfo(int max, int capacity) {
-        List<SeatsResponseDto> seats = new ArrayList<>();
-        for(int i = 0; i < capacity; i++) {
-            if(i < max) {
-                seats.add(new SeatsResponseDto(String.valueOf(i + 1), "VIP"));
-                continue;
-            }
-            seats.add(new SeatsResponseDto(String.valueOf(i + 1), "일반"));
-        }
-        return seats;
+                venues.getPossibleTimes(),
+                venues.getSeats());
     }
 }
