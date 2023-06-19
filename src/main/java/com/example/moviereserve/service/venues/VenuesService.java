cb627448180b6888b6be1bc383a253ca9ba2b172
CordHouse <qkqktl5310@gmail.com>
2023-06-20 04:37:14 +0900

    refactor : 리스트형태의 좌석 등록 추가

--- src/main/java/com/example/moviereserve/service/venues/VenuesService.java
+++ src/main/java/com/example/moviereserve/service/venues/VenuesService.java
@@ -1,9 +1,13 @@
 package com.example.moviereserve.service.venues;
 
+import com.example.moviereserve.dto.seat.SeatRegisterDto;
 import com.example.moviereserve.dto.venues.VenuesResponseDto;
 import com.example.moviereserve.dto.venues.VenuesSetupRequestDto;
+import com.example.moviereserve.entity.seat.Seat;
+import com.example.moviereserve.entity.user.User;
 import com.example.moviereserve.entity.venues.Venues;
 import com.example.moviereserve.entity.venues.VenuesType;
+import com.example.moviereserve.repository.SeatRepository;
 import com.example.moviereserve.repository.VenuesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,17 +17,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class VenuesService {
     private final VenuesRepository venuesRepository;
+    private final SeatRepository seatRepository;
 
     // 공연장 등록 -> 관리자만 되도록 설정
     @Transactional
-    public VenuesResponseDto venuesSetup(VenuesSetupRequestDto venuesSetupRequestDto) {
+    public VenuesResponseDto venuesSetup(VenuesSetupRequestDto venuesSetupRequestDto, User user) {
         Venues venues = new Venues(
                 venuesSetupRequestDto.getName(),
                 venuesSetupRequestDto.getCapacity(),
                 VenuesType.findVenuesType(venuesSetupRequestDto.getVenuesType()),
-                venuesSetupRequestDto.getStartTime(),
-                venuesSetupRequestDto.getEndTime());
+                venuesSetupRequestDto.getPossibleTimes(),
+                user);
         venuesRepository.save(venues);
+
+        for(SeatRegisterDto seatRegister : venuesSetupRequestDto.getSeats()) {
+            Seat seat = new Seat(seatRegister.getSeatNumber(), seatRegister.getSeatType(), user, venues);
+            seatRepository.save(seat);
+        }
 
         return new VenuesResponseDto().toDo(venuesSetupRequestDto);
     }
