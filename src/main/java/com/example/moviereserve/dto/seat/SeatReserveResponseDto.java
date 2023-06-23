b273ee3772c7dd9bbfd43650f5cb4081f1235b49
CordHouse <qkqktl5310@gmail.com>
2023-06-23 18:47:29 +0900

    refactor : 공연장 -> 공연으로 변경

--- src/main/java/com/example/moviereserve/dto/seat/SeatReserveResponseDto.java
+++ src/main/java/com/example/moviereserve/dto/seat/SeatReserveResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.moviereserve.dto.seat;
 
+import com.example.moviereserve.entity.performance.Performance;
 import com.example.moviereserve.entity.seat.Seat;
 import com.example.moviereserve.entity.venues.Venues;
 import lombok.AllArgsConstructor;
@@ -15,7 +16,7 @@ public class SeatReserveResponseDto {
     private long performanceId;
     private List<SeatInfoResponseDto> seats;
 
-    public SeatReserveResponseDto toDo(Venues venues, List<SeatInfoResponseDto> seat) {
-        return new SeatReserveResponseDto(venues.getId(), seat);
+    public SeatReserveResponseDto toDo(Performance performance, List<SeatInfoResponseDto> seat) {
+        return new SeatReserveResponseDto(performance.getId(), seat);
     }
 }
