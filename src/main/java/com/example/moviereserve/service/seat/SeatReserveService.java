a0e87a8ba0c2cfc494cd0b3ba00d713cc8ffe700
CordHouse <qkqktl5310@gmail.com>
2023-06-23 18:54:35 +0900

    refactor : 좌석 공연장 -> 공연으로 변경 및 잔여 좌석 카운트 추가

--- src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
+++ src/main/java/com/example/moviereserve/service/seat/SeatReserveService.java
@@ -6,10 +6,13 @@ import com.example.moviereserve.dto.seat.SeatInfoResponseDto;
 import com.example.moviereserve.dto.seat.SeatReserveRequestDto;
 import com.example.moviereserve.dto.seat.SeatReserveResponseDto;
 import com.example.moviereserve.entity.payment.Payment;
+import com.example.moviereserve.entity.performance.Performance;
 import com.example.moviereserve.entity.seat.Seat;
 import com.example.moviereserve.entity.user.User;
 import com.example.moviereserve.entity.venues.Venues;
+import com.example.moviereserve.exception.SeatReserveException;
 import com.example.moviereserve.repository.PaymentRepository;
+import com.example.moviereserve.repository.PerformanceRepository;
 import com.example.moviereserve.repository.SeatRepository;
 import com.example.moviereserve.repository.VenuesRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,16 +27,21 @@ import java.util.List;
 public class SeatReserveService {
     private static final String RESERVED = "occupied";
     private final SeatRepository seatRepository;
-    private final VenuesRepository venuesRepository;
+    private final PerformanceRepository performanceRepository;
     private final PaymentRepository paymentRepository;
 
     // 좌석 예매
     @Transactional
     public SeatReserveResponseDto reserve(SeatReserveRequestDto seatReserveRequestDto, User user) {
-        Venues venues = venuesRepository.findById(seatReserveRequestDto.getPerformanceId()).orElseThrow();
+        Performance performance = performanceRepository.findById(seatReserveRequestDto.getPerformanceId()).orElseThrow();
+        if(performance.getStatus() == 0) {
+            throw new SeatReserveException(); // 좌석이 다 찼습니다.
+        }
+        performance.setStatus(performance.getStatus()-1);
+
         List<SeatInfoResponseDto> seatInfoResponseDtoList = new ArrayList<>();
         for(SeatInfo reserve : seatReserveRequestDto.getSeats()) {
-            Seat seat = seatRepository.findBySeatNumberAndVenues(reserve.getSeatNumber(), venues).orElseThrow();
+            Seat seat = seatRepository.findBySeatNumberAndVenues(reserve.getSeatNumber(), performance.getVenues()).orElseThrow();
             seat.setUser(user);
             seat.setStatus(RESERVED);
 
@@ -41,7 +49,7 @@ public class SeatReserveService {
         }
 
         doPayment(seatReserveRequestDto.getPaymentInfo(), seatReserveRequestDto.getTotalPrice(), user);
-        return new SeatReserveResponseDto().toDo(venues, seatInfoResponseDtoList);
+        return new SeatReserveResponseDto().toDo(performance, seatInfoResponseDtoList);
     }
 
     @Transactional
